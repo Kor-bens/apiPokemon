e918e0d23599ae94c7a416ba70851167f65cdd2c
Tortosa <sofianetortosa@gmail.com>
2023-11-30 12:15:24 +0100

    maj

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ app.use(express.static(__dirname));
 
 
 // app.get("/", (req, res) => res.send("hello, express 38!"));
-app.get("/", (req, res) => res.sendFile('./index.html', { root: __dirname }));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 
 
